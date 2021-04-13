efe20dca63888705af74b3d3bd0ae51596f89c51
asnav <asnav1999@gmail.com>
2021-04-13 13:36:18 +0300

    Update main.py
    fixed pivoting to be called for each column instead of once in the beggining.
    added a checkpoint to check if the determinant of a matrix is zero, and print an error message if so (instead of trying to solve and eventually crashing).

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 def calcAnswer(matA, result):
     n = len(matA)
     vector = [0] * n
-    vector[n-1] = result[n-1][0]
-    for i in range(n - 2 , -1, -1):
+    vector[n - 1] = result[n - 1][0]
+    for i in range(n - 2, -1, -1):
         vector[i] = result[i][0]
-        for j in range(n - 1, i , -1):
+        for j in range(n - 1, i, -1):
             if matA[i][j] != 0:
                 vector[i] -= matA[i][j] * vector[j]
         vector[i] /= matA[i][i]
     return vector
 
-def detCalculator(matA): #it works!!!
+
+def detCalculator(matA):  # it works!!!
     matLen = len(matA)
     if matLen < 2:
         return None
@@ -32,54 +33,60 @@ def detCalculator(matA): #it works!!!
             sum += pivot * detCalculator(newMat)
         return sum
 
-def elementryInverse(matA, i, j = None):
+
+def elementryInverse(matA, i, j=None):
     if j is None:
         j = i
     if matA[i][j] != 1:
         matA[i][j] = -matA[i][j]
     return matA
-    
+
+
 def elementaryListMultiplicate(elementaryList, dim):
     X = makeIdentityMat(dim)
     for i in range(len(elementaryList)):
         X = matMultiply(X, elementaryList[i])
     return X
 
+
 def gaussianElimination(matA, result):
-    matA , result = pivoting(matA, result)
+    if detCalculator(matA) == 0:
+        return "Matrix unsolvable (zero or infinite results)"
     matA, result = makeUpperTriangular(matA, result)
     return calcAnswer(matA, result)
+
 
 def getElementaryList(matA, result):
     elementaryList = []
     dim = len(matA)
     for j in range(dim):
         for i in range(j):
-            print("I is: ", i)
-            print("J is: ", j)
             elementary = None
             if i != j:
-                matA,elementary = makePivotZero(j, i, matA, result, True)
+                matA, elementary = makePivotZero(j, i, matA, result, True)
                 elementaryList.append(elementary)
     return (matA, elementaryList)
+
 
 def inverse(mat):
     inverseMat = makeIdentityMat(len(mat))
     matCopy = [x[:] for x in mat]
     dim = len(mat)
     # for row in range(dim):
-        # if matCopy[row][row] == 0:
-        #for column in range(dim):
+    # if matCopy[row][row] == 0:
+    # for column in range(dim):
     return 0
+
 
 def LU(matA, result):
     dim = len(matA)
-    U, elementaryList =  getElementaryList(matA, result)
+    U, elementaryList = getElementaryList(matA, result)
     L = elementaryListMultiplicate(elementaryList, dim)
     print("U is:")
     printMat(U)
     print("L is:")
     printMat(L)
+
 
 def makeIdentityMat(dim):
     # Works for square mat only
@@ -94,9 +101,11 @@ def makeIdentityMat(dim):
         identityMat.append(rowMat)
     return identityMat
 
+
 def makeUpperTriangular(matA, result):
     dim = len(matA)
     for i in range(dim):
+        matA, result = pivoting(matA, i, result)
         for j in range(i + 1):
             if i == j:
                 matA, result = makePivotOne(i, matA, result)
@@ -104,23 +113,26 @@ def makeUpperTriangular(matA, result):
                 matA, result = makePivotZero(i, j, matA, result)
     return (matA, result)
 
-def makePivotZero(i, j, matA, result, LU = False):
+
+def makePivotZero(i, j, matA, result, LU=False):
     if matA[j][j] == 0:
         return matA
     I = makeIdentityMat(len(matA))
     I[i][j] = -matA[i][j] / matA[j][j]
-    if LU is True: 
-        return (matMultiply(I, matA),elementryInverse(I, i, j))
+    if LU is True:
+        return (matMultiply(I, matA), elementryInverse(I, i, j))
     return (matMultiply(I, matA), matMultiply(I, result))
 
-def makePivotOne(i, matA, result, LU = False):
+
+def makePivotOne(i, matA, result, LU=False):
     if matA[i][i] == 0:
         return matA
     I = makeIdentityMat(len(matA))
     I[i][i] = 1 / matA[i][i]
-    if LU is True: 
+    if LU is True:
         return elementryInverse(I, i)
     return (matMultiply(I, matA), matMultiply(I, result))
+
 
 def matMultiply(matA, matB):
     # multiply from the left: A * B.
@@ -135,6 +147,7 @@ def matMultiply(matA, matB):
         result.append(rowMat)
     return result
 
+
 def norm(matA):
     result = 0
     for row in matA:
@@ -145,28 +158,31 @@ def norm(matA):
             result = currentRowSum
     return result
 
-def pivoting(matA, result = None):
+
+def pivoting(matA, index, result=None):
     # find max pivot in each column
     n = len(matA)
-    for i in range(n):
-        for j in range(i,n):
-            if abs(matA[j][i]) > abs(matA[i][i]):
-                matA[i], matA[j] = matA[j],matA[i]
-                if result != None: 
-                    result[i], result[j] = result[j], result[i]
-    if result!= None:
+    for j in range(index, n):
+        if abs(matA[j][index]) > abs(matA[index][index]):
+            matA[index], matA[j] = matA[j], matA[index]
+            if result != None:
+                result[index], result[j] = result[j], result[index]
+    if result != None:
         return (matA, result)
     return matA
-        
+
+
 def printMat(mat):
     for row in mat:
-            print(row)
+        print(row)
+
 
 def startMatrixCalculation(matA, result):
     if len(matA) < 4:
         return gaussianElimination(matA, result)
     else:
         return LU(matA, result)
+
 
 # _______________________________________________________________
 
@@ -175,6 +191,13 @@ Q = [[1, 3, 5, 9],
      [4, 13, 9, 7],
      [5, 2, 3, 9]]
 
-C = [[11],[5],[4], [13]]
+C = [[11], [5], [4], [13]]
+
+
+Z = [[4, -2, 1],
+     [8, -4, 2],
+     [6, -1, -1]]
+
+D = [[3], [6], [1]]
 
 print(startMatrixCalculation(Q, C))
