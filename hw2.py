7773a69807609d711a0447bd4e96cbc1a2dc4bb3
David Abenhaim <68952247+ZiteiX@users.noreply.github.com>
2021-05-01 13:07:58 +0300

    Jacobi method added

--- hw2.py
+++ hw2.py
@@ -4,9 +4,11 @@ def main():
      [4, 13, 39, 7],
      [5, 1, 1, 9]]
     
-    B = [[2, 1, 0],
-         [4,6,1],
-         [22,1,27]]
+    B = [[4, 2, 0],
+         [2, 10, 4],
+         [0, 4, 5]]
+    
+    result = [[2], [6], [5]]
 
     options = {
         # should be functions instead.
@@ -14,11 +16,14 @@ def main():
         2: "Jacboi"
     }
     # do-while Python implementation.
-    while True:
-        choice = int(input("What is your choice: \n 1. Gauss Zaidel \n 2.Jacobi"))
-        if choice == 1 or choice == 2:
-            break
-    print(options[choice])
+    # while True:
+    #     choice = int(input("What is your choice: \n 1. Gauss Zaidel \n 2.Jacobi"))
+    #     if choice == 1 or choice == 2:
+    #         break
+    # print(options[choice])
+    print(B)
+    # jacobi(B, result)
+    
 
 def matMultiply(matA, matB):
     # multiply from the left: A * B.
@@ -48,12 +53,14 @@ def isDominantDiagonal(matA):
 
 # def makeDominantDiagonal(matA):
 
-def changeColum(matA, colA, colB):
+def changeCol(matA, colA, colB):
     elementaryMat = buildElementary(matA, colA, colB)
     return matMultiply(matA, elementaryMat)
 
-def changeRow(matA, rowA , rowB):
+def changeRow(matA, rowA , rowB, result = None):
     elementaryMat = buildElementary(matA, rowA, rowB)
+    if result != None:
+        return (matMultiply(elementaryMat, matA), matMultiply(elementaryMat, result))
     return matMultiply(elementaryMat, matA)
 
 def buildElementary(mat, a = None, b = None):
@@ -80,5 +87,29 @@ def buildElementary(mat, a = None, b = None):
                     lineMat.append(0)
         elementaryMat.append(lineMat)
     return elementaryMat
+
+def jacobi(matA, result, epsilion = 0.00001 ):
+    R1 = []
+    for i in range(len(matA)):
+        R1.append([0])
+    while True:
+        print(R1)
+        R2 = R1[:]
+        for i in range(len(matA)):
+            rowSum = 0
+            for j in range(len(matA[i])):
+                if j != i:
+                    rowSum += matA[i][j] * R1[j][0]
+            R2[i][0] = ((-rowSum + result[i][0]) / matA[i][i])
+            if(checkEpsilion(R1, R2, epsilion)):
+                break
+            R1 = R2[:]
+            print(R2)
+
+def checkEpsilion(R1, R2, epsilion):
+    for i in range(len(R1)):
+        if abs(R2[i][0]) - abs(R1[i][0]) > epsilion:
+            return False
+    return True
 
 main()
