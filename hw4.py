171e397218eea7e7c01dee07e6ba8e63e737a06d
David Abenhaim <68952247+ZiteiX@users.noreply.github.com>
2021-05-21 11:15:24 +0300

    HW4 - final commit. everything works as excpected.

--- hw4.py
+++ hw4.py
@@ -1,91 +1,137 @@
 import sympy as sp
-
+import numpy as np
 
 x = sp.symbols("x")
 
-
-def Bisection_Method(f, fTag, pStart, pEnd, section=0.1, epsilion=0.00000000001):
-    range = findSuspectPoint(f, pStart, pEnd, section)
-    answers = []
-    while range:
-        # pStart = range[1]
-        answers.append(bisection(f, range[0], range[1], epsilion))
-        range = findSuspectPoint(f, range[1], pEnd, section)
-
-    range = findSuspectPoint(fTag, pStart, pEnd, section)
-    while range:
-        pStart = range[1]
-        c = (bisection(fTag, range[0], range[1], epsilion))
-        print("c is: ", c)
-        if abs(f(c)) < epsilion:
-            answers.append(c)
-        range = findSuspectPoint(fTag, pStart, pEnd, section)
-    print(answers)
+METHODS = ["Bisection", "Newton Raphson", "Secant"]
+EPSILION = 0.0001
+SECTION = 0.1
 
 
-def bisection(f, a, b, epsilion):
-    i = 0
+def derivatie(f, x):
+    fTag = sp.diff(f, x)
+    return sp.lambdify(x, fTag)
+
+
+def Bisection_Method(f, a, b, epsilion=EPSILION):
+    maxIter = -(np.log(epsilion / (b - a))) / np.log(2)
+    iterations = 0
     while (b - a) > epsilion:
-        i += 1
+        if iterations > maxIter:
+            print("Max iterations limit.")
+            return None
         c = (a + b) / 2
         if f(a) * f(c) > 0:
             a = c
         else:
             b = c
-        # print("Iteration number: ", i, "f(a) = ", f(a), " ---- f(b) = ", f(b), " ---- f(c) = ", f(c), " ---- a = ", a, " ----- b = ", b, " ---- c = ", c)
-    return c
+        iterations += 1
+    return c, iterations
 
 
-def Newton_Raphson(f, fTag, pStart, pEnd, section=0.1, epsilion=0.0001):
-    print("I said good day")
+def Newton_Raphson(f, fTag, a, b, epsilion=EPSILION):
+    x0 = (a + b) / 2
+    x1 = a
+    iteration = 0
+    while abs(x1-x0) > epsilion:
+        if fTag(x0) == 0:
+            print("cant divided by zero")
+            break
+        temp = x1
+        x1 = x0-(f(x0)/fTag(x0))
+        x0 = temp
+        iteration += 1
+    return x1, iteration
 
 
-def secant_method(f, fTag, pStart, pEnd, section=0.1, epsilion=0.0001):
-    print("good day")
+def secant_method(f, a, b, epsilion=EPSILION):
+    x2 = (a + b) / 2
+    x1 = b
+    x0 = a
+    iteration = 0
+    while abs(x2 - x1) > epsilion:
+        if f(x1) - f(x0) == 0:
+            return None
+        temp = x2
+        x2 = (x0 * f(x1) - x1 * f(x0)) / (f(x1) - f(x0))
+        x0 = x1
+        x1 = temp
+        iteration += 1
+    return x1, iteration
 
 
-def findSuspectPoint(f, pStart, pEnd, section):
+def find_suspect_point(f, pStart, pEnd, section=SECTION):
     x = pStart + section
+    i = 0
     while x < pEnd:
+        i += 1
+        # print("Iteration number: ", i, " ---- x = ", x, " --- f(x) = ", f(x))
         if (f(x) * f(x - section)) < 0:
             return x - section, x
         x += section
     return False
 
 
+def start_calc(f, fTag, pStart, pEnd, method, section=SECTION, epsilion=EPSILION):
+    fx = sp.lambdify(x, f)
+    fTagx = sp.lambdify(x, fTag)
+    answers = []
+    methodName = METHODS[method-1]
+    root = []
+    # every root contain: [value, iterations till value reached]
+
+    area = find_suspect_point(fx, pStart, pEnd, section)
+
+    while area:
+        if method == 1:
+            root = (Bisection_Method(fx, area[0], area[1], epsilion))
+        if method == 2:
+            root = Newton_Raphson(fx, fTagx, area[0], area[1], epsilion)
+        if method == 3:
+            root = secant_method(fx, area[0], area[1], epsilion)
+        if root is not None:
+            answers.append(root)
+        area = find_suspect_point(fx, area[1], pEnd, section)
+
+    # using the derivative
+    area = find_suspect_point(fTagx, pStart, pEnd, section)
+    while area:
+        if method == 1:
+            root = (Bisection_Method(fTagx, area[0], area[1], epsilion))
+        if method == 2:
+            root = Newton_Raphson(fTagx, derivatie(fTag, x), area[0], area[1], epsilion)
+        if method == 3:
+            root = secant_method(fTagx, area[0], area[1], epsilion)
+        if root is not None:
+            if abs(fx(root[0])) < epsilion:
+                answers.append((abs(root[0]), root[1]))
+        area = find_suspect_point(fTagx, area[1], pEnd, section)
+
+    print("\n_________________")
+    print("Using", methodName, "method:")
+    for i in range(len(answers)):
+        print("The", i + 1, "root is:", "%.4f" % answers[i][0], "and the number of iterations are:", answers[i][1])
+    print("_________________ \n")
 
 
 def main():
-    polinom = x**4+x**3-3*x**2
-    derivative = sp.diff(polinom, x)
-    derivative = sp.lambdify(x, derivative)
-    polinom = sp.lambdify(x, polinom)
+    f = x ** 4 + x ** 3 - 3 * x ** 2
+    fTag = sp.diff(f, x)
     pStart = -2.5
     pEnd = 1.9
 
-    # print(polinom(1))
-    # print(derivative)
-
-
-    Bisection_Method(polinom, derivative, pStart, pEnd)
-
     while True:
-        choice = int(input(
+        method = int(input(
         "What is the method you want to use:\n" +
         "1. Bisection method\n" +
         "2. Newton Rapshon method\n" +
         "3. Secant method\n" +
         "4. Exit\n"))
 
-        if choice == 1:
-            Bisection_Method(polinom, pStart, pEnd)
-        if choice == 2:
-            Newton_Raphson(polinom, pStart, pEnd)
-        if choice == 3:
-            secant_method(polinom, pStart, pEnd)
-        if choice == 4:
-            print("Bye")
+        if method == 4:
+            print("Good day")
             break
+        start_calc(f, fTag, pStart, pEnd, method)
 
 
 main()
---
+++ last projects/main.py
@@ -0,0 +1,203 @@
+def calcAnswer(matA, result):
+    n = len(matA)
+    vector = [0] * n
+    vector[n - 1] = result[n - 1][0]
+    for i in range(n - 2, -1, -1):
+        vector[i] = result[i][0]
+        for j in range(n - 1, i, -1):
+            if matA[i][j] != 0:
+                vector[i] -= matA[i][j] * vector[j]
+        vector[i] /= matA[i][i]
+    return vector
+
+
+def detCalculator(matA):  # it works!!!
+    matLen = len(matA)
+    if matLen < 2:
+        return None
+    if matLen == 2:
+        return (matA[0][0] * matA[1][1]) - (matA[0][1] * matA[1][0])
+    else:
+        sum = 0
+        currentRow = 0
+        # will calculate det of first row
+        # We assume that its a square matrix
+        for currentCol in range(matLen):
+            newMat = [x[:] for x in matA]
+            for row in range(matLen):
+                del newMat[row][currentCol]
+            del newMat[currentRow]
+            pivot = matA[currentRow][currentCol]
+            if currentCol % 2 != 0:
+                pivot *= -1
+            sum += pivot * detCalculator(newMat)
+        return sum
+
+
+def elementryInverse(matA, i, j=None):
+    if j is None:
+        j = i
+    if matA[i][j] != 1:
+        matA[i][j] = -matA[i][j]
+    return matA
+
+
+def elementaryListMultiplicate(elementaryList, dim):
+    X = makeIdentityMat(dim)
+    for i in range(len(elementaryList)):
+        X = matMultiply(X, elementaryList[i])
+    return X
+
+
+def gaussianElimination(matA, result):
+    if detCalculator(matA) == 0:
+        return "Matrix unsolvable (zero or infinite results)"
+    matA, result = makeUpperTriangular(matA, result)
+    return calcAnswer(matA, result)
+
+
+def getElementaryList(matA, result):
+    elementaryList = []
+    dim = len(matA)
+    for j in range(dim):
+        for i in range(j):
+            elementary = None
+            if i != j:
+                matA, elementary = makePivotZero(j, i, matA, result, True)
+                elementaryList.append(elementary)
+    return (matA, elementaryList)
+
+
+def inverse(mat):
+    inverseMat = makeIdentityMat(len(mat))
+    matCopy = [x[:] for x in mat]
+    dim = len(mat)
+    # for row in range(dim):
+    # if matCopy[row][row] == 0:
+    # for column in range(dim):
+    return 0
+
+
+def LU(matA, result):
+    dim = len(matA)
+    U, elementaryList = getElementaryList(matA, result)
+    L = elementaryListMultiplicate(elementaryList, dim)
+    print("U is:")
+    printMat(U)
+    print("L is:")
+    printMat(L)
+
+
+def makeIdentityMat(dim):
+    # Works for square mat only
+    identityMat = []
+    for i in range(dim):
+        rowMat = []
+        for j in range(dim):
+            if i == j:
+                rowMat.append(1)
+            else:
+                rowMat.append(0)
+        identityMat.append(rowMat)
+    return identityMat
+
+
+def makeUpperTriangular(matA, result):
+    dim = len(matA)
+    for i in range(dim):
+        matA, result = pivoting(matA, i, result)
+        for j in range(i + 1):
+            if i == j:
+                matA, result = makePivotOne(i, matA, result)
+            else:
+                matA, result = makePivotZero(i, j, matA, result)
+    return (matA, result)
+
+
+def makePivotZero(i, j, matA, result, LU=False):
+    if matA[j][j] == 0:
+        return matA
+    I = makeIdentityMat(len(matA))
+    I[i][j] = -matA[i][j] / matA[j][j]
+    if LU is True:
+        return (matMultiply(I, matA), elementryInverse(I, i, j))
+    return (matMultiply(I, matA), matMultiply(I, result))
+
+
+def makePivotOne(i, matA, result, LU=False):
+    if matA[i][i] == 0:
+        return matA
+    I = makeIdentityMat(len(matA))
+    I[i][i] = 1 / matA[i][i]
+    if LU is True:
+        return elementryInverse(I, i)
+    return (matMultiply(I, matA), matMultiply(I, result))
+
+
+def matMultiply(matA, matB):
+    # multiply from the left: A * B.
+    result = []
+    for i in range(len(matA)):
+        rowMat = []
+        for j in range(len(matB[i])):
+            index = 0
+            for k in range(len(matB)):
+                index += matA[i][k] * matB[k][j]
+            rowMat.append(index)
+        result.append(rowMat)
+    return result
+
+
+def norm(matA):
+    result = 0
+    for row in matA:
+        currentRowSum = 0
+        for column in matA[row]:
+            currentRowSum += abs(matA[row][column])
+        if currentRowSum > result:
+            result = currentRowSum
+    return result
+
+
+def pivoting(matA, index, result=None):
+    # find max pivot in each column
+    n = len(matA)
+    for j in range(index, n):
+        if abs(matA[j][index]) > abs(matA[index][index]):
+            matA[index], matA[j] = matA[j], matA[index]
+            if result != None:
+                result[index], result[j] = result[j], result[index]
+    if result != None:
+        return (matA, result)
+    return matA
+
+
+def printMat(mat):
+    for row in mat:
+        print(row)
+
+
+def startMatrixCalculation(matA, result):
+    if len(matA) < 4:
+        return gaussianElimination(matA, result)
+    else:
+        return LU(matA, result)
+
+
+# _______________________________________________________________
+
+Q = [[1, 3, 5, 9],
+     [1, 12, 1, 7],
+     [4, 13, 9, 7],
+     [5, 2, 3, 9]]
+
+C = [[11], [5], [4], [13]]
+
+
+Z = [[4, -2, 1],
+     [8, -4, 2],
+     [6, -1, -1]]
+
+D = [[3], [6], [1]]
+
+print(startMatrixCalculation(Q, C))
